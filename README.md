3a1e375e34ead5d809ec5bb0fb76b9c75c037523
Vikram S. Negi <viknegi0@gmail.com>
2025-03-25 04:54:56 +0530

    Custom template and CSS for post.

--- README.md
+++ README.md
@@ -1,10 +1,10 @@
 # Blog
 
-A static blog site to share my thoughts with the world.
-
-Note: Contact EMAIL_ID to request for content removal.
+A static blog site to share my thoughts with the world. Contact [vikram.s.negi@proton.me](mailto:vikram.s.negi@proton.me) if you have any issues with my content.
 
 ## Markdown Metadata
+
+Make sure all your blog posts (markdown files) have this metadata. This helps provide structure to the document.
 
 ```yaml
 ---
@@ -15,8 +15,19 @@ description: "A short summary of the blog post."
 ---
 ```
 
+## Create a Post
+
+Simply create a directory with two things:
+
+1. A markdown file with any name.
+2. A directory for assets named `assets/`.
+
+Note: The name of the directory should be in lowercase and hypen separated. As it will be used as the HTML filename.
+
 ## TODO
 
-- [ ] Input a post directory `["article.md", "assets/*"]`
-- [ ] Let python access the markdown metadata (post title)
-- [ ] Add post to index.html
+- [*] Input a blog-post directory `["article.md", "assets/*"]`
+- [ ] Let python access the markdown metadata (post title and date)
+- [ ] Add post title and date to index.html
+- [ ] Image compression
+- [ ] index.html page
--- main.py
+++ main.py
@@ -1,24 +1,71 @@
 import os
 import sys
 import subprocess
+import shutil
+
 
 def convert_markdown_to_html(markdown_file):
-    md_file = os.path.abspath(markdown_file)
-    print(f"Path: {md_file}")
+    print(f"MD Path: {markdown_file}")
+    base_dir = os.path.dirname(markdown_file)
+    html_filename = os.path.basename(base_dir)
+
     try:
-        subprocess.run(["pandoc", md_file, "-o", "temp/post.html", "--template=template.html"], check=True)
+        subprocess.run(["pandoc", markdown_file, "-o", f"posts/{html_filename}.html", "--template=template.html"], check=True)
         print("Success: Markdown to HTML conversion complete.")
     except Exception as e:
         print(f"Error: {e}")
         sys.exit(1)
 
+
+def check_valid_post_dir(blog_post_dir):
+    files_in_dir = os.listdir(blog_post_dir)
+    return ("assets" in files_in_dir) and (len(files_in_dir) == 2)
+
+
+def get_markdown_file(blog_post_dir):
+    for file in os.listdir(blog_post_dir):
+        if file.endswith(".md"):
+            return os.path.join(blog_post_dir, file)
+    return None
+
+
+def copy_asset_files(blog_post_dir):
+    assets_dir = os.path.join(blog_post_dir, "assets")
+    destination_asset_dir = "posts/assets"
+
+    for asset in os.listdir(assets_dir):
+        src_path = os.path.join(assets_dir, asset)
+        dst_path = os.path.join(destination_asset_dir, asset)
+
+        if os.path.isfile(src_path):
+            shutil.copy(src_path, dst_path)
+
+
 def main():
-    markdown_file = sys.argv[1]
+    blog_post_dir = sys.argv[1]
+    if not os.path.isdir(blog_post_dir):
+        print(f"Error: {blog_post_dir} is not a valid directory.")
+        sys.exit(1)
+    
+    blog_post_dir = os.path.abspath(blog_post_dir)
+    is_valid_dir = check_valid_post_dir(blog_post_dir)
+    
+    if not is_valid_dir:
+        print(f"Error: Make sure the blog post directory has only two items article.md and assets/")
+        sys.exit(1)
+
+    markdown_file = get_markdown_file(blog_post_dir)
+    if markdown_file is None:
+        print("Markdown file not found.")
+        sys.exit(1)
+
     convert_markdown_to_html(markdown_file)
+    copy_asset_files(blog_post_dir)
+
 
 if __name__ == "__main__":
     if len(sys.argv) != 2:
-        print("Usage: python main.py <markdown_file_path>")
+        print("Usage: python main.py <blog_post_directory>")
         sys.exit(1)
     
     main()
